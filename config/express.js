6cc77d7f1f42f553b18a2a2bf2f41430d652fac9
Noah Prail <nprail@filiosoft.com>
2018-02-17 15:22:02 -0500

    Added helmet

--- config/express.js
+++ config/express.js
@@ -1,8 +1,12 @@
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
+const helmet = require('helmet')
+const pkg = require('../package.json')
 
 module.exports = app => {
+  app.use(helmet())
+
   // Use body parser so we can get info from POST and/or URL parameters
   app.use(bodyParser.urlencoded({
     extended: true
@@ -12,8 +16,9 @@ module.exports = app => {
   // Allow CORS
   app.use(cors())
   app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    // res.header('Access-Control-Allow-Origin', '*')
+    // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    res.header('Server', `s3d-api (${pkg.version})`)
     next()
   })
 
