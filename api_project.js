96cb97ebb59cdc8ef66aa3b5b8d3d8fe9e71e6a6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-02-18 22:12:24 +0000

    Deploy Filiosoft/maxup-api to github.com/Filiosoft/maxup-api.git:gh-pages

--- api_project.js
+++ api_project.js
@@ -3,13 +3,13 @@ define({
   "url": "https://api.maxup.sh",
   "sampleUrl": "https://api.maxup.sh",
   "name": "maxup-api",
-  "version": "0.0.1",
+  "version": "0.0.1-canary.0",
   "description": "⚙ The API for maxup!",
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-02-18T19:17:07.260Z",
+    "time": "2018-02-18T22:12:09.773Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
